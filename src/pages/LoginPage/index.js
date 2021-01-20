0df4fc9ae16023ae381f680c667ef5fc605512ec
wesam <mr0virus@gmail.com>
2021-01-20 13:57:31 +0200

    login page + usestate

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import logo from '../../assets/images/logo.png';
 import { Link } from 'react-router-dom';
 
 
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        console.log(email, password);
+    }
 
   return (
     <div className="login">
@@ -18,13 +24,13 @@ const LoginPage = () => {
             <h1 className="login__title">Login</h1>
             <div className="login_inputContainer">
                 <p className="login__inputLabel">Email</p>
-                <input type="email" name="email" />
+                <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className="login_inputContainer">
                 <p className="login__inputLabel">Passwrd</p>
-                <input type="password" name="passwrod" />
+                <input type="password" name="passwrod" onChange={(e) => setPassword(e.target.value)} />
             </div>
-            <button onClick="">Login</button>
+            <button onClick={handleLogin}>Login</button>
             <p className="login__cardText">Don't have an account!,<Link to="/signup">Sign up</Link> now.</p>
             <p className="login__cardText">By continuing, you agree to JOCO's Conditions of Use and Privacy Notice.</p>
           </div>
